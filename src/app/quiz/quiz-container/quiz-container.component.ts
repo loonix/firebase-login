aa43448cb90e384a49592087e3cae969ed32e3fe
Daniel Carneiro <geral4x@gmail.com>
2018-10-02 14:04:32 +0100

    minor update to quiz, not fully working

--- src/app/quiz/quiz-container/quiz-container.component.ts
+++ src/app/quiz/quiz-container/quiz-container.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { QuizService } from '../services/quiz.service';
+import { quizConfig } from '../quiz.config';
+import { Quiz } from '../models/quiz.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz-container',
@@ -7,9 +13,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizContainerComponent implements OnInit {
 
-  constructor() { }
+  items: Observable<any[]>;
+  tasks: Observable<any[]>;
 
-  ngOnInit() {
+  myQuiz: string;
+  editMode: boolean;
+  taskToEdit: any = {};
+
+  constructor(private db: AngularFirestore, private quizService: QuizService) {
+    this.items = db.collection('quiz').valueChanges();
   }
 
+  ngOnInit(): void {
+    this.tasks = this.db.collection(quizConfig.collection_endpoint).valueChanges();
+
+    this.tasks = this.db
+      .collection(quizConfig.collection_endpoint)
+      .snapshotChanges()
+      .pipe(
+        map(actions => {
+          return actions.map(a => {
+            // Get document data
+            const data = a.payload.doc.data() as Quiz;
+            // Get document id
+            const id = a.payload.doc.id;
+            // Use spread operator to add the id to the document data
+            return { id, ...data };
+          });
+        })
+      );
+  }
+
+  onEdit(task) {
+    console.log(task);
+    // Set taskToEdit and editMode
+    this.taskToEdit = task;
+    this.editMode = true;
+    // Set form value
+    this.myQuiz = task.description;
+  } // edit
+
+  onSave() {
+    if (this.myQuiz !== null) {
+      // Get the input value
+      const task = {
+        description: this.myQuiz
+      };
+      if (!this.editMode) {
+        console.log(task);
+        this.quizService.addTask(task);
+      } else {
+        // Get the task id
+        const taskId = this.taskToEdit.id;
+        // update the task
+        this.quizService.updateTask(taskId, task);
+      }
+      // set edit mode to false and clear form
+      this.editMode = false;
+      this.myQuiz = '';
+    }
+  } // saveTask
+
+  onDelete(quiz) {
+    // Get the task id
+    const quizId = quiz.id;
+    // delete the task
+    this.quizService.deleteTask(quizId);
+  } // deleteTask
 }
+
--- src/app/quiz/services/quiz.service.ts
+++ src/app/quiz/services/quiz.service.ts
@@ -1,9 +1,33 @@
+import { quizConfig } from '../quiz.config';
+import { Quiz } from '../models/quiz.model';
 import { Injectable } from '@angular/core';
-
+import {
+  AngularFirestoreDocument,
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
+  tasks: AngularFirestoreCollection<Quiz>;
+  private taskDoc: AngularFirestoreDocument<Quiz>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.tasks = db.collection<Quiz>(quizConfig.collection_endpoint);
+  }
+
+  addTask(task) {
+    this.tasks.add(task);
+  }
+
+  updateTask(id, update) {
+    this.taskDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
+    this.taskDoc.update(update);
+  }
+
+  deleteTask(id) {
+    this.taskDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
+    this.taskDoc.delete();
+  }
 }
