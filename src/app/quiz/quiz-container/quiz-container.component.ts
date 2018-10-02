94a5ee380ef63f10fa8a63440a91ceddfa4af4e9
Daniel Carneiro <geral4x@gmail.com>
2018-10-02 22:41:54 +0100

    added bootstrap, font awesome and refactored quiz

--- src/app/quiz/quiz-container/quiz-container.component.ts
+++ src/app/quiz/quiz-container/quiz-container.component.ts
@@ -9,76 +9,77 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-quiz-container',
   templateUrl: './quiz-container.component.html',
-  styleUrls: ['./quiz-container.component.css']
+  styleUrls: ['./quiz-container.component.scss']
 })
 export class QuizContainerComponent implements OnInit {
-
-  items: Observable<any[]>;
-  tasks: Observable<any[]>;
-
-  myQuiz: string;
+  quiz: Observable<any[]>;
+  question: string;
+  status: string;
+  author: string;
+  image: string;
+  category: string;
   editMode: boolean;
-  taskToEdit: any = {};
+  quizToEdit: any = {};
 
-  constructor(private db: AngularFirestore, private quizService: QuizService) {
-    this.items = db.collection('quiz').valueChanges();
-  }
+  constructor(private db: AngularFirestore, private quizService: QuizService) {}
 
   ngOnInit(): void {
-    this.tasks = this.db.collection(quizConfig.collection_endpoint).valueChanges();
+    this.quiz = this.db
+      .collection(quizConfig.collection_endpoint)
+      .valueChanges();
 
-    this.tasks = this.db
+    this.quiz = this.db
       .collection(quizConfig.collection_endpoint)
       .snapshotChanges()
       .pipe(
         map(actions => {
           return actions.map(a => {
-            // Get document data
             const data = a.payload.doc.data() as Quiz;
-            // Get document id
             const id = a.payload.doc.id;
-            // Use spread operator to add the id to the document data
             return { id, ...data };
           });
         })
       );
   }
 
-  onEdit(task) {
-    console.log(task);
-    // Set taskToEdit and editMode
-    this.taskToEdit = task;
+  onEdit(quiz) {
+    console.log(quiz);
+    this.quizToEdit = quiz;
     this.editMode = true;
-    // Set form value
-    this.myQuiz = task.description;
-  } // edit
+
+    this.question = quiz.question;
+    this.status = quiz.status;
+    this.author = quiz.author;
+    this.image = quiz.image;
+    this.category = quiz.category;
+  }
 
   onSave() {
-    if (this.myQuiz !== null) {
-      // Get the input value
-      const task = {
-        description: this.myQuiz
-      };
-      if (!this.editMode) {
-        console.log(task);
-        this.quizService.addTask(task);
-      } else {
-        // Get the task id
-        const taskId = this.taskToEdit.id;
-        // update the task
-        this.quizService.updateTask(taskId, task);
-      }
-      // set edit mode to false and clear form
-      this.editMode = false;
-      this.myQuiz = '';
+    const quiz = {
+      question: this.question,
+      status: this.status,
+      author: this.author,
+      image: this.image,
+      category: this.category
+    };
+    if (!this.editMode) {
+      console.log(quiz);
+      this.quizService.addQuiz(quiz);
+    } else {
+      const quizId = this.quizToEdit.id;
+      this.quizService.updateQuiz(quizId, quiz);
     }
-  } // saveTask
+    this.editMode = false;
+
+    this.question = '';
+    this.status = '';
+    this.author = '';
+    this.image = '';
+    this.category = '';
+  }
 
   onDelete(quiz) {
-    // Get the task id
     const quizId = quiz.id;
-    // delete the task
-    this.quizService.deleteTask(quizId);
-  } // deleteTask
+    this.quizService.deleteQuiz(quizId);
+  }
 }
-
--- src/app/quiz/services/quiz.service.ts
+++ src/app/quiz/services/quiz.service.ts
@@ -10,24 +10,24 @@ import {
   providedIn: 'root'
 })
 export class QuizService {
-  tasks: AngularFirestoreCollection<Quiz>;
-  private taskDoc: AngularFirestoreDocument<Quiz>;
+  quiz: AngularFirestoreCollection<Quiz>;
+  private quizDoc: AngularFirestoreDocument<Quiz>;
 
   constructor(private db: AngularFirestore) {
-    this.tasks = db.collection<Quiz>(quizConfig.collection_endpoint);
+    this.quiz = db.collection<Quiz>(quizConfig.collection_endpoint);
   }
 
-  addTask(task) {
-    this.tasks.add(task);
+  addQuiz(task): void {
+    this.quiz.add(task);
   }
 
-  updateTask(id, update) {
-    this.taskDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
-    this.taskDoc.update(update);
+  updateQuiz(id, update): void {
+    this.quizDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
+    this.quizDoc.update(update);
   }
 
-  deleteTask(id) {
-    this.taskDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
-    this.taskDoc.delete();
+  deleteQuiz(id): void {
+    this.quizDoc = this.db.doc<Quiz>(`${quizConfig.collection_endpoint}/${id}`);
+    this.quizDoc.delete();
   }
 }
